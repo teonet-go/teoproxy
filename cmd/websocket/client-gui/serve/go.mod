b1ce08e95fdc3913376e4be78c221b59db88379b
Kirill Scherba <kirill@scherba.ru>
2024-01-04 17:49:00 +0300

    Add htts and lets encrypt. #4

--- cmd/websocket/client-gui/serve/go.mod
+++ cmd/websocket/client-gui/serve/go.mod
@@ -1,3 +1,5 @@
 module github.com/teonet-go/teoproxy/cmd/websocket/client-gui/serve
 
 go 1.21.5
+
+require golang.org/x/crypto v0.14.0 // indirect
--- cmd/websocket/client-gui/serve/main.go
+++ cmd/websocket/client-gui/serve/main.go
@@ -35,32 +35,45 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/teonet-go/teoproxy/teonet/server"
+	"golang.org/x/crypto/acme/autocert"
 )
 
 const (
 	appShort = "client-gui-serve"
 )
 
+var domain string
+
+// main is the entry point of the program.
+//
+// It parses application parameters, defines a handler function for HTTP requests,
+// registers the handler function to handle all requests, creates a file server
+// to serve static files, registers a websocket server, starts an HTTPS server
+// if a domain is set, or starts an HTTP server if a domain is not set.
 func main() {
-	// Define a handler function for the HTTP requests
+
+	// Parse application parameters
+	var laddr string
+	//
+	flag.StringVar(&domain, "domain", "", "domain name to process HTTP/s server")
+	flag.StringVar(&laddr, "laddr", "localhost:8081", "local address of http, used if domain doesn't set")
+	flag.Parse()
+
+	// Define Hello handler function for the HTTP requests
 	handler := func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, World!")
 	}
 
-	// Register the handler function to handle all requests
+	// Register the Hello handler function to handle all requests
 	http.HandleFunc("/hello", handler)
 
 	// Create a file server to serve static files from the "wasm" directory
-	// fs := http.FileServer(http.Dir("wasm"))
-	// http.Handle("/", http.StripPrefix("/", fs))
-
-	// TODO: when embedded filesystem on
-	// Register the file server handler to handle all requests
-	// http.Handle("/", http.FileServer(http.FS(fs)))
 	frontendFS := http.FileServer(http.FS(getFrontendAssets()))
 	http.Handle("/", frontendFS)
 
@@ -72,9 +85,32 @@ func main() {
 	}
 	http.HandleFunc("/ws", serve.HandleWebSocket)
 
-	// Start the web server and listen on port 8080
-	err = http.ListenAndServe(":8081", nil)
-	if err != nil {
-		fmt.Println("Server error:", err)
+	// Start HTTPS server if domain is set
+	if len(domain) > 0 {
+
+		// Redirect HTTP requests to HTTPS
+		go func() {
+			err := http.ListenAndServe(":80", http.HandlerFunc(redirectTLS))
+			if err != nil {
+				log.Fatalf("ListenAndServe error: %v", err)
+			}
+		}()
+
+		// Start HTTPS server and create certificate for domain
+		log.Println("Start https serve with domain:", domain)
+		log.Fatal(http.Serve(autocert.NewListener(domain), nil))
+		return
 	}
+
+	// Start HTTP server
+	log.Println("Start http serve at:", laddr)
+	log.Fatalln(http.ListenAndServe(laddr, nil))
+}
+
+// redirectTLS redirects the HTTP request to HTTPS.
+//
+// It takes in the http.ResponseWriter and http.Request as parameters.
+func redirectTLS(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "https://"+domain+":443"+r.RequestURI,
+		http.StatusMovedPermanently)
 }
