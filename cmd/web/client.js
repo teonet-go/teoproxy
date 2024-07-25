f2c225dfb62826296f569e5187e7d1c5e3fc48c2
Kirill Scherba <kirill@scherba.ru>
2024-07-25 10:17:49 +0300

    Commants added to the web example

--- cmd/web/client.js
+++ cmd/web/client.js
@@ -1,14 +1,17 @@
-
+// Import TeoProxyClient class and Command enum
 import TeoProxyClient from "./teoproxy.js";
 import { Command } from "./teoproxy.js";
 
+// The teoFortune Teonet peer name
 const teoFortune = "8agv3IrXQk7INHy5rVlbCxMWVmOOCoQgZBF";
 
+// Create TeoProxy client object
 let teo = new TeoProxyClient();
 
-// console.debug("connect to:", teoFortune);
+// Connect to Teonet proxy websocket and Teonet peer api.
 teo.connect("fortune-gui.teonet.dev", teoFortune, nextMessage);
 
+// On message received from Teonet peer
 teo.onmessage = (pac) => {
     console.debug("onmessage", pac);
     if (pac.cmd == Command.SendTo) {
@@ -16,6 +19,7 @@ teo.onmessage = (pac) => {
     }
 }
 
+// Send next message request
 export function nextMessage() {
     teo.cmd.sendTo(teoFortune, "fortb");
 }
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,48 +3,13 @@ package main
 import (
 	"log"
 	"net/http"
-
-	"github.com/gorilla/websocket"
 )
 
 func main() {
 
-	// Start web server
+	// Handle static files in the current directory
 	http.Handle("/", http.FileServer(http.Dir("./")))
 
-	// Websocket handler
-	http.HandleFunc("/ws", handleWebSocket)
-
+	// Start web server
 	log.Fatal(http.ListenAndServe(":8093", nil))
-
-}
-
-func handleWebSocket(w http.ResponseWriter, r *http.Request) {
-
-	log.Println("ws client start connection", r.RemoteAddr)
-
-	var upgrader = websocket.Upgrader{} // use default options
-
-	c, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		log.Print("upgrade:", err)
-		return
-	}
-	defer c.Close()
-
-	log.Println("ws client connected", c.RemoteAddr())
-
-	for {
-		mt, message, err := c.ReadMessage()
-		if err != nil {
-			log.Println("read:", err)
-			break
-		}
-		log.Printf("recv: %s", message)
-		err = c.WriteMessage(mt, message)
-		if err != nil {
-			log.Println("write:", err)
-			break
-		}
-	}
 }
